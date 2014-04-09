78962504b8f9fea290dedb8c07adbdcd29dd1d90
administrator <administrator@administrators-Mac-mini.local>
2014-04-09 06:13:43 -0400

    chore(init): Initial commit to the repository

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-genforce
-========
+enforce-generators
+=======
+Module to enforce harmony generator support for applications even in older versions of node
 
-Genforce enables/enforces harmony-generator support on an application
+Tested on node version 0.8, 0.10, and 0.11
+
+This module is only meant to be used on the entry point of your application. Please do not use it
+on internal modules.
+
+Usage
+----------
+    npm install --save enforce-generators
+    var enforce = require('enforce-generators');
+    enforce('path/to/main/script');
+
+**NOTE:**
+>
+> This module uses [gnode](https:/github.com/tootallnate/gnode) to transpile generator code to ES5 code for node version
+ < 0.11.3
+> This can cause large applications to slow-down considerably
+
+> It's not recommended to use this in a small library as the dependency weight comes in at around
+5MB due to the use of regenerator. It's better instead to inform your users of the requirements,
+check for support, and exit if they don't have the needed generator support.
---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var path = require('path');
+
+// this module checks to see if generator syntax is supported
+var supported = require('generator-supported');
+
+var genforce = function(filePath){
+  // this is the main entry point to the application
+  var mainPath = path.dirname(require.main.filename);
+
+  // this is the main file for the application
+  var filePath = path.resolve(mainPath,  filePath);
+
+  // if generators are supported we can simply require it and return
+  if(supported){
+    require(filePath);
+    return true;
+  }
+
+  // we need to see if generators are available in the current version of node
+  var version = process.version;
+  var required = "v0.11.3";
+
+  var semver = require('semver');
+
+  if(semver.lt(version, required)){
+    require('gnode');
+    return true;
+  }
+
+  // here we have node 0.11.3 or higher so we just need to turn on the harmony flag
+  var spawn = require('child_process').spawn;
+
+  // get the current arguments
+  var args = process.argv;
+
+  var cmd = 'node';
+
+  // pop off the binary and the filepath
+  args.shift();
+  args.shift();
+
+  // add the proper filepath to avoid the extra requires again
+  args.unshift(filePath);
+  args.unshift("--harmony");
+
+  // set the io for the child thread
+  var opts = {
+    customFds: [ 0, 1, 2 ],
+    stdio: 'inherit'
+  };
+
+  var child = spawn(cmd, args, opts);
+  child.on('exit', onexit);
+
+  return true;
+};
+
+function onexit(code, signal) {
+  if (signal) {
+    process.kill(process.pid, signal);
+  } else {
+      process.exit(code);
+  }
+}
+
+// this is just to make sure they don't try to run the wrong file
+if(require.main === module) {
+  console.log('You can\'t run this script directly');
+}
+
+module.exports = enforce;
